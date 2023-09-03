48ff1e276b5a41f80808cc48bfe6f430b8b1a86b
Kees van Beilen <76178905+Kees-van-Beilen@users.noreply.github.com>
2023-09-03 13:08:02 +0200

    fixed url in bindings.ts

--- bindings/bindings.ts
+++ bindings/bindings.ts
@@ -20,7 +20,7 @@ const opts = {
   name: "deno_audio",
   url:
     (new URL(
-      "https://github.com/littledivy/deno_audio/releases/download/0.2.0",
+      "https://github.com/Kees-van-Beilen/deno_audio/releases/download/0.2.0",
       import.meta.url,
     )).toString(),
   suffixes: {
